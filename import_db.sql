e41e8034605729c5de50fb1b4b3d2a5e12d8aded
zahiahmed <zahi.ahmed33@gmail.com>
2022-12-07 11:22:22 -0500

    replies table zahi drive, mlkz nav

--- import_db.sql
+++ import_db.sql
@@ -2,7 +2,7 @@ CREATE TABLE users(
   id INTEGER PRIMARY KEY,
   fname VARCHAR(255) NOT NULL, -- TODO: Do we need NOT NULL?
   lname VARCHAR(255) NOT NULL
-)
+);
 
 CREATE TABLE questions(
   id INTEGER PRIMARY KEY,
@@ -11,8 +11,22 @@ CREATE TABLE questions(
   author_id INTEGER NOT NULL,
 
   FOREIGN KEY (author_id) REFERENCES users(id)
-)
+);
 
 CREATE TABLE question_follows(
   id INTEGER PRIMARY KEY, -- TODO: DO we need it 
-)
+  user_id INTEGER NOT NULL,
+  question_id INTEGER NOT NULL,
+
+  FOREIGN KEY (user_id) REFERENCES users(id)
+  FOREIGN KEY (question_id) REFERENCES questions(id)
+);
+
+CREATE TABLE replies(
+  id INTEGER PRIMARY KEY,
+  subject VARCHAR(255) NOT NULL,
+  body TEXT NOT NULL,
+  user_id INTEGER,
+  
+  parent_reply_id INTEGER
+);
